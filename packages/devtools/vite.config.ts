9cd616818537c450bf58deb917c8b893a576ed41
glennmichael123 <gtorregosa@gmail.com>
2026-03-22 21:28:17 +0800

    fix: resolve typecheck errors

--- packages/devtools/vite.config.ts
+++ packages/devtools/vite.config.ts
@@ -1,12 +1,10 @@
 import { resolve } from 'node:path'
 import vue from '@vitejs/plugin-vue'
-import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [
     vue(),
-    UnoCSS(),
   ],
   resolve: {
     alias: {
